9de8e8c329983d123c4b0c27415ed10971fdadee
Caseiros-Dellights <151055013+Caseiros-Dellights@users.noreply.github.com>
2023-11-16 17:13:04 -0300

    Add files via upload

--- src/components/MyDichesStyle.js
+++ src/components/MyDichesStyle.js
@@ -31,7 +31,7 @@ export const ImageContainer = styled.div`
 
     @media screen and (max-width: 768px) {
         width: 100%;
-        height: 270px;
+        height: 300px;
         img {
             height: 100%;
         }
